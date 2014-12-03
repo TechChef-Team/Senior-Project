7177e0a742c3f64d70e0430cc3ee1f743723f3f2
Mohammad AL-Rashed <alrashedmohammad91@gmail.com>
2014-12-03 19:04:12 +0300

    >>

--- TechChef/app/src/main/java/com/senior/techchef/RestaurantView.java
+++ TechChef/app/src/main/java/com/senior/techchef/RestaurantView.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class restaurant extends Activity {
+public class RestaurantView extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
